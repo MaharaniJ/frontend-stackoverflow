6edc9c35f2d01669464bb63d447e386d26ef388e
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-07-10 15:08:51 +0530

    /

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://backend-stackoverflow-x2hh.onrender.com/',
+      target: 'https://backend-stackoverflow-x2hh.onrender.com',
       changeOrigin: true,
     })
   );
