276d06e1e223a4dc5f94e7853ed4a7134dbb6e2f
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-08 15:17:23 +0530

    update-ans-views-votes

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,54 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import FilterListIcon from '@mui/icons-material/FilterList';
-import './main.css'
-import AllQuestions from '../AllQuestions/AllQuestions';
-import { Link } from 'react-router-dom';
-import SideBar from '../Sidebar/SideBar';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import "./main.css";
+import AllQuestions from "../AllQuestions/AllQuestions";
+import { Link } from "react-router-dom";
+import SideBar from "../Sidebar/SideBar";
+import axios from "axios";
 
 function Main(props) {
-    const [questions,setQuestions] = useState([]);
-    const [loading,setLoading] = useState(true);
-    // console.log(questions.tags)
-    useEffect(()=>{
-        async function fetchData(){
-            await axios.get('https://backend-stackoverflow-x2hh.onrender.com/api/question').then((res)=> {
-                setLoading(false);
-                setQuestions(res.data);
-                // console.log(res.data);
-                
-            });
-            
-        }
-        fetchData()
-    } , []);
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // console.log(questions.tags)
+  useEffect(() => {
+    async function fetchData() {
+      await axios
+        .get("https://backend-stackoverflow-x2hh.onrender.com/api/question")
+        .then((res) => {
+          setLoading(false);
+          setQuestions(res.data);
+          // console.log(res.data);
+        });
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
-    <SideBar />
-    <div className="main">
+      <SideBar />
+      <div className="main">
         <div className="top">
-            <h2>{props.questions}</h2>
-            <Link to='/AskQuestion'>Ask question</Link>
+          <h2>{props.questions}</h2>
+          <Link to="/AskQuestion">Ask question</Link>
         </div>
         <div className="bottom">
-            <p className="total-qus">{questions.length} questions</p>
-            <div className="btn">
-                <a href='/' className='button'>Newest</a>
-                <a href='/' className='button'>Active</a>
-                <a href='/' className='button'>More</a>
-                <a href='/' className="filter button"><FilterListIcon /> Filter</a>
-            </div>
+          <p className="total-qus">{questions.length} questions</p>
+          <div className="btn">
+            <a href="/" className="button">
+              Newest
+            </a>
+            <a href="/" className="button">
+              Active
+            </a>
+            <a href="/" className="button">
+              More
+            </a>
+            <a href="/" className="filter button">
+              <FilterListIcon /> Filter
+            </a>
+          </div>
         </div>
-        
+
         {/* <AllQuestions /> */}
-        {loading?"Loading....": questions && questions.map((question) =><AllQuestions key={question._id} tags={question.tags} title={question.title} user={question.user} createdAt={question.createdAt} _id={question._id} views={question.views} answers={question.answers}/>)}
-        </div>
+        {loading
+          ? "Loading...."
+          : questions &&
+            questions.map((question) => (
+              <AllQuestions
+                key={question._id}
+                tags={question.tags}
+                title={question.title}
+                user={question.user}
+                createdAt={question.createdAt}
+                _id={question._id}
+                views={question.views}
+                answers={question.answers}
+              />
+            ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
 Main.defaultProps = {
-    questions:"Top Questions",
-}
+  questions: "Top Questions",
+};
