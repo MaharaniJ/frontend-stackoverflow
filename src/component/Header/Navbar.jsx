0a882d2074de2fffd8ce5896081e7d4112caa5a3
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-08 11:54:35 +0530

    stackoverflow logo

--- src/component/Header/Navbar.jsx
+++ src/component/Header/Navbar.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 // import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import InboxIcon from '@mui/icons-material/Inbox';
-import './navbar.css'
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../../features/userSlice';
-import { Avatar } from '@mui/material';
+import InboxIcon from "@mui/icons-material/Inbox";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
+import { Avatar } from "@mui/material";
+
 
 function Navbar() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
   // console.log(user)
   return (
     <>
       <div className="content">
         <div className="left-content">
-          <Link to='/'>
-            <img src= "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/768px-Stack_Overflow_icon.svg.png (max-width: 768w), https://stackoverflow.design/assets/img/logos/so/logo-stackoverflow.svg 1200w" alt='logo' />
+          <Link to="/">
+            <img
+              src="https://stackoverflow.design/assets/img/logos/so/logo-stackoverflow.svg"
+              alt="logo"
+            />
           </Link>
           <p>Products</p>
         </div>
@@ -27,11 +31,14 @@ function Navbar() {
         <div className="right-content">
           <Avatar src={user?.photo} />
           <InboxIcon />
-          <img src="https://stackoverflow.design/assets/img/logos/se/se-icon.svg" alt="Dropdown" />
+          <img
+            src="https://stackoverflow.design/assets/img/logos/se/se-icon.svg"
+            alt="Dropdown"
+          />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
