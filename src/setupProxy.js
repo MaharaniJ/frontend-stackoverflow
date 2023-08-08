91c005c324cb523f6ef5480edd1757f7f6118732
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-08 12:32:30 +0530

    proxy-settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
-    '/api',
+    "/api",
     createProxyMiddleware({
-      target: 'https://backend-stackoverflow-x2hh.onrender.com',
+      target: "https://backend-stackoverflow-x2hh.onrender.com",
       changeOrigin: true,
     })
   );
