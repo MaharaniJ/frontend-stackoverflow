dce9c7c87de3a64c4054130b692f7475ed2fdae2
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-08 13:40:35 +0530

    change-setupproxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      // target:"https://kaleidoscopic-jalebi-2cc6e1.netlify.app",
-       target: "https://backend-stackoverflow-x2hh.onrender.com",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       // target:"https://kaleidoscopic-jalebi-2cc6e1.netlify.app",
+//        target: "https://backend-stackoverflow-x2hh.onrender.com",
+//       changeOrigin: true,
+//     })
+//   );
+// };
 
 ///45652153902-7u7s0jul996r28lrfinocr89sg37e31g.apps.googleusercontent.com
 ///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
